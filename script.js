0af9b5464e499ecbdac69c0f3bb145fdfc36988b
AnoopSahu95 <anoopgotha@gmail.com>
2026-02-27 18:42:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function scrollToPlans() {
+    document.getElementById("plans").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(current)) {
+            link.classList.add("active");
+        }
+    });
+});
